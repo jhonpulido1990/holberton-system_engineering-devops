325e312280b9ba0f7d74a8bed0e0495257cbd483
jhonpulido1990 <3825@holbertonschool.com>
2022-04-27 17:55:21 -0500

    task advance 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,39 @@
+# Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
+exec {'apt update':
+  command => '/usr/bin/apt-get -y update',
+}
+
+exec {'apt upgrade':
+  command => '/usr/bin/apt-get -y upgrade',
+}
+
+package {'nginx':
+  ensure  => installed,
+  require => Exec[apt update],
+}
+
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_inline {'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $HOSTNAME;'
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
