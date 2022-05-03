a25f3110eac233cf084e742291e5513ce157615d
jhonpulido1990 <3825@holbertonschool.com>
2022-05-02 22:43:14 -0500

    new change

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Using strace, find out why Apache is returning a 500 error. Once you find the issue                                                        
-                                                                                            
-exec { 'correction_server':                                                                 
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",                                                                                     
-  path    => '/bin',                                                                        
-}
+# Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet                                                          
+                                                                                                                                                                                         
+exec { 'correction_server':                                                                                                                                                              
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",                                                                                      
+  path    => '/bin',                                                                                                                                                                     
+} 
