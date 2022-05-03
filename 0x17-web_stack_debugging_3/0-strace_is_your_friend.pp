7571af708708210ddebbdb9cf502e4a19d9cb640
jhonpulido1990 <3825@holbertonschool.com>
2022-05-03 10:50:04 -0500

    coorection puppet-link

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet                                                          
                                                                                                                                                                                          
-exec { 'correction_server':                                                                                                                                                              
+exec { 'correctionserver':                                                                                                                                                              
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",                                                                                      
   path    => '/bin',                                                                                                                                                                     
 } 
