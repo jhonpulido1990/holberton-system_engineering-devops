8ee38f7bd4a053035b8a09e79fdfef972681c50b
jhonpulido1990 <3825@holbertonschool.com>
2022-05-05 22:01:01 -0500

    delecte space

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
+# config file nging debugin 4
+
 exec { 'this will output stuff':
   path    => '/bin',
   command => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx;  sudo service nginx restart',
-
 }
