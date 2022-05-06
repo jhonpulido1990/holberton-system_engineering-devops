3eb9b082761e94923247581a8a31fc8f64e1b441
jhonpulido1990 <3825@holbertonschool.com>
2022-05-05 22:38:43 -0500

    change start neginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
 # config file nging debugin 4
-
 exec { 'this will output stuff':
-  path    => '/bin',
-  command => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx;  sudo service nginx restart',
+  path      => '/bin',
+  command   => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx',
+  logoutput => true,
+}
+
+exec { 'nging restart':
+    path    => '/etc/init.d',
+    command => 'nginx restart'
 }
