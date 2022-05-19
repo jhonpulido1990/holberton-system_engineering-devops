db4e8c334265894c1258b80cae8f28f9b822e8a7
jhonpulido1990 <3825@holbertonschool.com>
2022-05-19 17:14:22 -0500

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""using this REST API,
+for a given employee ID,
+return list progress"""
+import json
+import requests
+
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users')
+    user_json = user.json()
+    afterdic = {}
+    for name in user_json:
+        afterdic[name["id"]] = name["username"]
+    task = ""
+    dicci = {}
+    lista = []
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for t in todos.json():
+            dictio = {}
+            dictio["task"] = t["title"]
+            dictio["completed"] = t["completed"]
+            dictio["username"] = afterdic[t["userId"]]
+            lista.append(dictio)
+            dicci[t["userId"]] = lista
+    filename =  "todo_all_employees.json"
+    with open(filename, "w") as outfile:
+        json.dump(dicci, outfile)
