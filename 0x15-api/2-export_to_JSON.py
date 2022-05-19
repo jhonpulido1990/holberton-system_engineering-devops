de181de1c2a4d6d908492d385fb311fcec8d7ce5
jhonpulido1990 <3825@holbertonschool.com>
2022-05-19 15:17:35 -0500

    add name file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -24,5 +24,5 @@ if __name__ == "__main__":
             dicci[t["userId"]] = lista
     print(dicci)
 
-with open("sample.json", "w") as outfile:
+with open("todo_all_employees.json", "w") as outfile:
     json.dump(dicci, outfile)
