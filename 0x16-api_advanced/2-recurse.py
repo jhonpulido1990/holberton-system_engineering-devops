ad6da1c96286c6253fe237d4527184d7a143a4f2
jhonpulido1990 <3825@holbertonschool.com>
2022-05-21 10:32:05 -0500

    task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Reddit API and returns a list containing the
+titles of all hot articles
+'''
+from requests import get
+after = ''
+
+
+def recurse(subreddit, hot_list=[]):
+    ''' recursive function to obtain a list of top titles in all pages '''
+    global after
+    header = {'User-Agent': 'My User Agent 1.0'}
+    try:
+        url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+        if after:
+            url = url + '?after={}'.format(after)
+        getting = get(url, headers=header, allow_redirects=False).json()
+        for children in getting['data']['children']:
+            hot_list.append(children.get('data').get('title'))
+        after = get(url, headers=header,
+                    allow_redirects=False).json().get('data').get('after')
+        if after:
+            return recurse(subreddit, hot_list)
+        return hot_list
+    except:
+        return None
